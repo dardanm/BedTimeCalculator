7f57a2afa21fd977e7deea84bb8366c824af2b27
Dardan Meha <mdardan20@gmail.com>
2018-02-27 17:29:36 -0500

    Setup buttons

--- app/src/main/java/com/meha/dardan/bedtimecalculator/MainActivity.java
+++ app/src/main/java/com/meha/dardan/bedtimecalculator/MainActivity.java
@@ -8,15 +8,57 @@ import android.support.v4.app.DialogFragment;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private Button timePickerButton;
+    private Button wakeUpButtons[] = new Button[9];
+    private Button sleepButtons[] = new Button[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wakeUpButtons[0] = (Button) findViewById(R.id.wakeUpButtonOne);
+        wakeUpButtons[1] = (Button) findViewById(R.id.wakeUpButtonTwo);
+        wakeUpButtons[2] = (Button) findViewById(R.id.wakeUpButtonThree);
+        wakeUpButtons[3] = (Button) findViewById(R.id.wakeUpButtonFour);
+        wakeUpButtons[4] = (Button) findViewById(R.id.wakeUpButtonFive);
+        wakeUpButtons[5] = (Button) findViewById(R.id.wakeUpButtonSix);
+        wakeUpButtons[6] = (Button) findViewById(R.id.wakeUpButtonSeven);
+        wakeUpButtons[7] = (Button) findViewById(R.id.wakeUpButtonEight);
+        wakeUpButtons[8] = (Button) findViewById(R.id.wakeUpButtonNine);
+
+        sleepButtons[0] = (Button) findViewById(R.id.sleepButtonOne);
+        sleepButtons[1] = (Button) findViewById(R.id.sleepButtonTwo);
+        sleepButtons[2] = (Button) findViewById(R.id.sleepButtonThree);
+        sleepButtons[3] = (Button) findViewById(R.id.sleepButtonFour);
+        sleepButtons[4] = (Button) findViewById(R.id.sleepButtonFive);
+        sleepButtons[5] = (Button) findViewById(R.id.sleepButtonSix);
+        sleepButtons[6] = (Button) findViewById(R.id.sleepButtonSeven);
+        sleepButtons[7] = (Button) findViewById(R.id.sleepButtonEight);
+        sleepButtons[8] = (Button) findViewById(R.id.sleepButtonNine);
+
+        Calendar c = Calendar.getInstance();
+        int currentHour = c.get(Calendar.HOUR_OF_DAY);
+        int currentMinutes = c.get(Calendar.MINUTE);
+
+        boolean isPM = (currentHour >= 12);
+       // String currentTime = (String.format("%02d:%02d %s", (currentHour == 12 || currentHour == 0) ? 12 : currentHour % 12, currentMinutes, isPM ? "PM" : "AM"));
+
+        //wakeUpButtons[0].setText(currentTime);
+
+        for (int i = 0; i<wakeUpButtons.length;i++){
+            String currentTime = (String.format("%02d:%02d %s", (currentHour == 12 || currentHour == 0) ? 12 : currentHour % 12, currentMinutes, isPM ? "PM" : "AM"));
+            wakeUpButtons[i].setText(currentTime);
+
+
+        }
+
+
     }
 
     public void showTimePickerDialog(View view) {
